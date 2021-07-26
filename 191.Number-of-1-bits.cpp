7a6c04dffbf1cb71f0c4a3bab46a96b04d46919e
ZeroDeath <zero.death.0313@gmail.com>
2021-07-26 14:23:38 +0530

    number of 1 bits c++ program

---
+++ 191.Number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int total=0;
+        while(n)
+        {
+            if((n&1)==1)
+            {
+                total++;
+            }
+            n >>= 1;
+        }
+        return total;
+    }
+};
