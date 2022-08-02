92a17c2b18f308c1c1fe9656fb34b395db26a4dd
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-08-02 16:51:00 +0530

    Update 198. House Robber.cpp

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -1,3 +1,4 @@
+//DP+Memoization
 class Solution {
 public:
     int solve(int id,vector<int>& nums,vector<int>&dp)
@@ -22,5 +23,49 @@ public:
         int n=nums.size();
         vector<int>dp(n,-1);
         return solve(n-1,nums,dp);
+    }
+};
+
+//Tabulation
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            int take=nums[i];
+            if(i>1)
+            {
+                take+=dp[i-2];
+            }
+            int nottake=0+dp[i-1];
+            dp[i]=max(take,nottake);
+        }
+        return dp[n-1];
+    }
+};
+
+//Tabulation+SpaceOptimization
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=0;i<n;i++)
+        {
+            int take=nums[i];
+            if(i>1)
+            {
+                take+=prev2;
+            }
+            int nottake=0+prev;
+            int curr=max(take,nottake);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
     }
 };
