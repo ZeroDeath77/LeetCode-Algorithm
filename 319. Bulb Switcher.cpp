6b230e7a22bb2dc5390e133ffc8f63a5ea94f56c
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-08-25 12:17:14 +0530

    Create 319. Bulb Switcher.cpp

---
+++ 319. Bulb Switcher.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        int cnt=0;
+        int i=1;
+        while(i*i<=n)
+        {
+            cnt++;
+            i++;
+        }
+        return cnt;
+    }
+};
